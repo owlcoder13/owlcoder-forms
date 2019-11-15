e43c5e81c5722a77f6d61b919816873e5d8d68e2
owl <owlcoder1@gmail.com>
2019-11-15 13:48:11 +0300

    dev

--- src/Fields/CheckBoxField.php
+++ src/Fields/CheckBoxField.php
@@ -4,5 +4,27 @@ namespace Owlcoder\Forms\Fields;
 
 class CheckBoxField extends Field
 {
-    public $template = 'forms/checkbox-field';
+    public function renderInput()
+    {
+        $attributes = $this->buildInputAttributes([
+            'type' => 'hidden',
+            'class' => '',
+            'value' => '0',
+            'id' => '',
+        ]);
+
+        $attributesActive = [
+            'type' => 'checkbox',
+            'class' => '',
+            'value' => '1',
+        ];
+
+        if ($this->value) {
+            $attributesActive['checked'] = 1;
+        }
+
+        $attributesActive = $this->buildInputAttributes($attributesActive);
+
+        return "<input $attributes >" . "<input $attributesActive >";
+    }
 }
